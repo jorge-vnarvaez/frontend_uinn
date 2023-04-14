25548d9d7fc7b16c6d4e7f53a3a9e74bd6f3c52d
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2023-04-14 10:11:00 -0400

    chore: page-loader design

--- components/PageLoader.vue
+++ components/PageLoader.vue
@@ -1,68 +1,33 @@
 <template>
   <div class="loader-container">
-    <p class="loader-text">
-      <span class="loader-letter u">U</span>
-      <span class="loader-letter i">I</span>
-      <span class="loader-letter n1">N</span>
-      <span class="loader-letter n2">N</span>
-    </p>
+    <div class="flex flex-col align-center">
+      <v-img src="/uinn-logo.png" width="80" height="40" contain ></v-img>
+
+      <v-divider class="my-6"></v-divider>
+
+      <span>
+        Cargando {{ percentage }}%
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    percentage: {
+      type: Number,
+      default: 0,
+    },
+  }
+};
 </script>
 
 <style>
 .loader-container {
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   height: 100vh;
-}
-
-.loader-text {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.loader-letter {
-  font-size: 36px;
-  font-weight: bold;
-  margin: 0 2px;
-  color: #ccc;
-}
-
-.u {
-  animation: color-change 2s ease-in-out 0.5s infinite;
-  color: #3498db;
-}
-
-.i {
-  animation: color-change 2s ease-in-out 0.5s infinite;
-  color: #2ecc71;
-}
-
-.n1 {
-  animation: color-change 2s ease-in-out 1s infinite;
-  color: #f1c40f;
-}
-
-.n2 {
-  animation: color-change 2s ease-in-out 1.5s infinite;
-  color: #e74c3c;
-}
-
-@keyframes color-change {
-  0% {
-    color: #ccc;
-  }
-  50% {
-    color: inherit;
-  }
-  100% {
-    color: #ccc;
-  }
 }
 </style>
