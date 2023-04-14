7f5970b9141c05dfaa2ce446468f5a773ca2a540
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2023-04-14 10:14:53 -0400

    chore: group slider funcionality

--- components/block/GroupSlider.vue
+++ components/block/GroupSlider.vue
@@ -1,10 +1,12 @@
 <template>
-  <v-row justify="space-between">
-    <!-- <v-col cols="4" lg="2" class="bg-red-200">4 columnas</v-col>
-    <v-col cols="3" lg="6" class="bg-green-200">3 columnas</v-col> -->
-
-    <!-- <BlockComponent v-for="block in blocks" :key="block.id" :block="block"></BlockComponent> -->
-  </v-row>
+  <div id="wrapper">
+        <BlockComponent
+          v-for="block in blocks"
+          :key="block.id"
+          :block="block"
+          id="item"
+        ></BlockComponent>
+  </div>
 </template>
 
 <script>
@@ -18,4 +20,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="postcss">
+
+#item {
+  @apply flex flex-wrap;
+}
+
+#wrapper {
+  @apply snap-x space-x-8;
+  @apply pb-20;
+  @apply flex;
+  overflow-x: scroll;
+}
+
+#wrapper::-webkit-scrollbar {
+  height: 8px;
+}
+
+#wrapper::-webkit-scrollbar-track {
+  background: #f1f5f9;
+}
+#wrapper::-webkit-scrollbar-thumb {
+  background: #cbd5e1;
+  border-radius: 5px;
+  max-width: 100px;
+}
+</style>
