6fbc4455dc24bf4a15a316a523aeb9e870e857b1
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2023-04-14 10:13:59 -0400

    add: percentage loader method

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,18 @@
 <template>
   <v-app>
-      <div v-if="!contentWasRendered">
-        <PageLoader />
-      </div>
-      <div v-if="contentWasRendered">
-        <NavHeader />
-        <Nuxt :style="uiSettingsLoaded ? ui_settings : {}" />
-        <NavFooter />
-      </div>
+    <script
+      charset="utf-8"
+      type="text/javascript"
+      src="//js.hsforms.net/forms/embed/v2.js"
+    ></script>
+    <div v-if="!contentWasRendered">
+      <PageLoader :percentage="progress" />
+    </div>
+    <div v-if="contentWasRendered">
+      <NavHeader />
+      <Nuxt :style="uiSettingsLoaded ? ui_settings : {}" />
+      <NavFooter />
+    </div>
   </v-app>
 </template>
 
@@ -17,21 +22,35 @@ export default {
   data() {
     return {
       uiSettingsLoaded: false,
-      contentWasRendered: false
+      contentWasRendered: false,
+      totalTime: 1000,
+      startTime: 0,
+      progress: 0,
     };
   },
   async beforeMount() {
-    await Promise.all([this.$store.dispatch("ui/loadUiSettings")]).then((res) => {
-      this.uiSettingsLoaded = true;
-      setTimeout(() => {
-        this.contentWasRendered = true;
-      }, 300)
-    })
+    this.startTime = new Date().getTime();
+    await Promise.all([this.$store.dispatch("ui/loadUiSettings")]).then(
+      (res) => {
+        this.uiSettingsLoaded = true;
+        let interval = setInterval(() => {
+          let currentTime = new Date().getTime();
+          let timeElapsed = currentTime - this.startTime;
+
+          this.progress = Math.round((timeElapsed / this.totalTime) * 100);
+          if (this.progress >= 100) {
+            this.progress = 100;
+            clearInterval(interval);
+            this.contentWasRendered = true;
+          }
+        }, 100);
+      }
+    );
   },
   computed: {
     ui_settings() {
-      return this.$store.getters['ui/getUiSettings'];
-    }
-  }
+      return this.$store.getters["ui/getUiSettings"];
+    },
+  },
 };
 </script>
