9db47413116a171eb73853573108f07d7ca43573
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2023-04-17 11:26:18 -0400

    fix: setActiveHeroImage mutation

--- pages/acerca_nuestro.vue
+++ pages/acerca_nuestro.vue
@@ -10,25 +10,6 @@
         </BlockComponent>
       </v-row>
 
-      <!-- <v-row class="pb-20">
-        <v-col cols="12" lg="4" v-for="core_value in core_values" :key="core_value.id" class="pr-4 pb-20">
-          <div>
-            <div class="w-6/12">
-                <span class="block font-gotham text-[18px] uppercase">
-                  {{ core_value.title }}
-                </span>
-
-                <v-divider class="my-4"></v-divider>
-            </div>
-          </div>
-
-          <span class="block font-inter text-[18px] w-10/12">
-            {{ core_value.excerpt }}
-          </span>
-        </v-col>
-      </v-row>
-
-      <v-divider></v-divider> -->
     </v-container>
   </div>
 </template>
@@ -41,7 +22,7 @@ export default {
     this.$store.commit("ui/setActiveParentType", "page")
 
     if (this.page.header) {
-      this.$store.commit("ui/setActiveHeroImage", this.$config.apiUrlV2 + "/assets/" + this.page.header.hero_image.id);
+      this.$store.commit("ui/setActiveHeroImage", this.$config.apiUrlV2 + "/assets/" + this.page.header.hero_image);
     }
   },
   mounted() {},
