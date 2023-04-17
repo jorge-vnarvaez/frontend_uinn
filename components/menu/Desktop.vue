507ed74d464f1a44ce167f7e4ee73291c28eeea3
jorge-vnarvaez <jorge.vnarvaez@gmail.com>
2023-04-17 11:23:29 -0400

    add: hoverable nav items

--- components/menu/Desktop.vue
+++ components/menu/Desktop.vue
@@ -1,23 +1,25 @@
 <template>
-  <div>
-    <div
-      class="flex space-x-20 justify-center align-center"
-    >
-      <nuxt-link
-        v-for="item in list"
-        :key="item.id"
-        :to="item.href"
-        class="text-slate-900"
-      >
-        <span @mouseover="servicesCard = false">
-          {{ item.title }}
-        </span>
-      </nuxt-link>
-    </div>
-    <!-- Menu -->
+<div>
+  <div class="flex space-x-20 justify-center align-center">
+    <nuxt-link v-for="item in list" :key="item.id" :to="item.href" class="text-slate-900">
+      <div  @mouseover="item.hoverable ? servicesCard = true : servicesCard = false">
+        <span>{{ item.title }}</span>
+        <div v-if="item.hoverable && servicesCard && item.hoverable" class="absolute bg-white shadow-2xl mt-6 z-10 top-10 rounded-lg" @mouseleave="servicesCard = false; childrenOnHover = null">
+          <div class="px-4 py-2 rounded-lg" v-for="children in item.childrens" :key="children.slug">
+            <nuxt-link :to="children.href" class="flex justify-between lg:space-x-6">
+              <span  @mouseover="childrenOnHover = children.slug"  class="text-slate-900 hover:font-bold">
+                {{ children.title }}
+              </span>
 
-    
+              <v-icon :class="[childrenOnHover == children.slug ? 'visible font-bold' : 'collapse']" small>mdi-arrow-top-right</v-icon>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </nuxt-link>
   </div>
+  <!-- Menu -->
+</div>
 </template>
 
 <script>
@@ -28,6 +30,12 @@ export default {
       default: () => [],
     },
   },
+  data() {
+    return {
+      childrenOnHover: false,
+      servicesCard: false,
+    };
+  }
 };
 </script>
 
--- components/menu/Mobile.vue
+++ components/menu/Mobile.vue
@@ -1,10 +1,10 @@
 <template>
   <div v-if="drawer" class="fixed top-0 left-0 w-full h-full z-50">
     <div
-      class="bg-white w-full max-w-md rounded-b-lg shadow-lg z-50"
+      class="bg-white w-full max-w-screen-xl mx-auto rounded-b-lg shadow-lg z-50"
       :style="ui_settings"
     >
-      <v-container>
+      <v-container class="px-0">
         <div class="flex justify-between align-center">
           <nuxt-link to="/">
             <v-img
@@ -15,7 +15,7 @@
               @click="$store.commit('ui/setNavigationDrawerState', false)"
             ></v-img>
           </nuxt-link>
-          <v-icon @click="$store.commit('ui/setNavigationDrawerState', false)"
+          <v-icon @click="$store.commit('ui/setNavigationDrawerState', false); parentActive = null"
             >mdi-close</v-icon
           >
         </div>
@@ -24,18 +24,40 @@
           <v-divider></v-divider>
         </div>
 
-        <div class="flex flex-col">
+        <div v-if="parentActive">
+          <div class="flex pb-4">
+            <v-icon @click="parentActive = null">mdi-chevron-left</v-icon>
+            <span class="block font-bold">{{ parent.title }}</span>
+          </div>
+
+          <v-divider></v-divider>
+
+          <div class="pt-4" v-if="parent.childrens.length > 0">
+            <div v-for="children in parent.childrens" :key="children.slug" class="py-2">
+              <nuxt-link :to="children.href">
+                <span class="block text-sm text-slate-500">{{ children.title }}</span>
+              </nuxt-link>
+            </div>
+          </div>
+        </div>
+
+        <div v-if="!parentActive" class="flex flex-col">
           <nuxt-link
             v-for="item in list"
             :key="item.id"
             :to="{ path: item.href }"
             class="pb-2"
           >
-            <span
-              class="text-slate-900"
-              @click="$store.commit('ui/setNavigationDrawerState', false)"
-              >{{ item.title }}</span
-            >
+            <div class="flex justify-between">
+              <span
+                class="text-slate-900"
+                @click="$store.commit('ui/setNavigationDrawerState', false)"
+                >{{ item.title }}</span
+              >
+
+              <v-icon v-if="item.hoverable" @click="parentActive = item.id">mdi-chevron-down</v-icon>
+              <v-icon v-if="item.hoverable && parentActive != null" @click="parentActive = null">mdi-chevron-up</v-icon>
+            </div>
           </nuxt-link>
         </div>
       </v-container>
@@ -52,7 +74,15 @@ export default {
       default: () => [],
     },
   },
+  data() {
+    return {
+      parentActive: null,
+    }
+  },
   computed: {
+    parent() {
+      return this.list.find((item) => item.id === this.parentActive);
+    },
     drawer() {
       return this.$store.getters["ui/getNavigationDrawerState"];
     },
